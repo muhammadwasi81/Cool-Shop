793e27b47a173c5696f68f9391a41db0a4e50478
Muhammad wasi <wasiarain819@gmail.com>
2021-09-02 17:11:18 +0500

    create: app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import { StoreProvider } from '../components/Store';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
+
+
+//TODO: IT WILL LOAD THE PAGE WHEN SWITCH ONE TO ANOTHER
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.getInitialState = async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  }
+}
